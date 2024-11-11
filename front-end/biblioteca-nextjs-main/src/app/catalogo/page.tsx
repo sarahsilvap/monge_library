0c4e726d3cc355627f7129d1a180268870336e23
sarahp <sarahs-pereira@outlook.com>
2024-11-11 14:37:14 -0300

    general fix

--- front-end/biblioteca-nextjs-main/src/app/catalogo/page.tsx
+++ front-end/biblioteca-nextjs-main/src/app/catalogo/page.tsx
@@ -7,14 +7,14 @@ import Carousel from "@/components/Carousel";
 const Catalogo = () => {
   return (
     <div className="bg-gray-100 py-10">
-      <h1 className="text-3xl text-center font-bold">Catálogo de Livros</h1>
-      <p className="text-center mt-5 text-lg">
+      <h1 className="text-2xl text-center font-medium">Catálogo de Livros</h1>
+      <p className="text-center mt-5 text-base">
         Explore nossa vasta coleção de livros, e-books e outros materiais.
         Encontre seu próximo livro favorito agora!
       </p>
       <div>
-        <h1 className="absolute left-10 mb-4">Novidades</h1>
-        <Carousel className="mt-6" />
+        <h1 className="absolute left-10 font-medium text-lg">Novidades</h1>
+        <Carousel />
       </div>
        
       <Carousel /> 
